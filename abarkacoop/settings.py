35dcbfca2bfcea6d4ba5bb66c1c102c452f0650e
Sergio Forcen <sergiocalipso@gmail.com>
2021-11-16 14:28:47 +0100

    Creacion de apps: orders, products, stakeholders

--- abarkacoop/settings.py
+++ abarkacoop/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "whitenoise.runserver_nostatic",
+    "orders",
+    "products",
+    "stakeholders",
 
-    "hello",
 ]
 
 MIDDLEWARE = [
