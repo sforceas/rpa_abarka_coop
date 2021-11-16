7e833eba48284823ce569cd31195a8267ad5f4d1
Sergio Forcen <sergiocalipso@gmail.com>
2021-11-16 15:29:49 +0100

    Añade modelo de ingredientes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from products.models import Ingredient
+
 # Register your models here.
+# Inlime models
+
+class IngredientInline(admin.StackedInline):
+    model = Ingredient
+    extra=0
+    can_delete = False
+    verbose_name = 'Ingrediente'
+    verbose_name_plural = 'Ingredientes'
+
+# Register your models here.
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Ingredient admin"""
+    list_display = ('name','ingredient_type','price_kg','active_flag') # Campos que debe mostrar en el display de admin
+    list_display_links=('name',) # Elementos linkados al detalle
+    list_editable=('price_kg',) # Elementos editables desde admin
+
+    search_field= ('name','description','ingredient_type')
+    list_filter = ('active_flag','ingredient_type','allergens')
+    readonly_fields = ('created','modified')
+
+#inlines = [IngredientInline,]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.db.models.fields import CharField, DecimalField, IntegerField, BooleanField, DateTimeField
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+INGREDIENT_TYPES = [
+    ('fruit','Fruta'),
+    ('vegetable','Verduras'),
+    ('cereal','Cereales'),
+    ('legume','Legumbres'),
+    ('dried_fruit','Frutos secos'),
+    ('meat','Carnes'),
+    ('fish','Pescados y mariscos'),
+    ('lactic','Lácticos'),
+    ('eggs', 'Huevo y derivados'),
+    ('species','Especias y condimentos'),
+    ('infusions','Infusiones'),
+    ('oils','Aceites y grasas vegetales'),
+    ('water','Agua'),
+    ('others','Otros'),
+    ]
+
+CONSERVATION_METHODS = [
+    ('storage','Almacén'),
+    ('nevera','Nevera'),
+    ('freezer','Congelador'),
+]
+
+ALLERGENS =[
+    ('spicy','Picante'),
+    ('gluten','Gluten'),
+    ('dried_fruits','Frutos secos'),
+    ('mollusks','Moluscos'),
+    ('fish','pescado'),
+    ('crustacean','Crustáceos'),
+    ('sesame','Sésamo'),
+    ('lactosa','Lactosa'),
+    ('mustard','Mostaza'),
+    ('celery','Apio'),
+    ('peanut','Cacahuete'),
+    ('sulfit','Sulfitos'),
+    ('soy','Soja'),
+    ('lupins','Altramuces'),
+]
+class Ingredient(models.Model):
+    """Ingredient model"""
+    name=models.CharField(verbose_name='Nombre',max_length=80)
+    description=CharField(verbose_name='Descripción',blank=True, max_length=300)
+    provider=CharField(verbose_name='Proveedor',max_length=80) #Sustituir por Provider
+
+    kcal = IntegerField(verbose_name='Calorías (kcal/100g)',blank=True)
+    ingredient_type = CharField(verbose_name='Tipo de ingrediente',choices=INGREDIENT_TYPES,max_length=30)
+    allergens = ArrayField(
+        CharField(choices=ALLERGENS, blank=True,max_length=30),verbose_name='Alérgenos')
+    lifetime = IntegerField(verbose_name='Vida útil (días)',blank=True)
+    price_kg=DecimalField(verbose_name='Precio por kg',blank=True,max_digits=7,decimal_places=2)
+    
+    active_flag=BooleanField(verbose_name='Activo',default=True)
+    created=DateTimeField(verbose_name='Creado',auto_now_add=True)
+    modified=DateTimeField(verbose_name='Modificado',auto_now=True)
+
+
+    def __str__(self):
+        """Return title."""
+        return f'{self.name} de {self.provider}'
